d1fea955aa5cd0d189bdb5db0d2f8b3b90524036
lokka30 <lokka30@protonmail.com>
2020-05-09 09:31:25 +0800

    v1.0.0-ALPHA

--- src/main/java/io/github/lokka30/phantomlib/objects/CachedTitle.java
+++ src/main/java/io/github/lokka30/phantomlib/objects/CachedTitle.java
@@ -9,14 +9,14 @@ public class CachedTitle {
 
     private String mainTitle;
     private String subTitle;
-    private int fadeIn;
-    private int stay;
-    private int fadeOut;
+    private int fadeIn = 5;
+    private int stay = 40;
+    private int fadeOut = 5;
 
     /**
      * Store a complete Title (main title, sub title, fade in, stay, fade out) in a single object
      *
-     * @param mainTitle the uppermost text
+     * @param mainTitle the top text
      * @param subTitle  the bottom text
      * @param fadeIn    the amount in ticks to fade in the title
      * @param stay      the amount in ticks to keep the title displayed
@@ -28,6 +28,16 @@ public class CachedTitle {
         this.fadeIn = fadeIn;
         this.stay = stay;
         this.fadeOut = fadeOut;
+    }
+
+    /**
+     * Store part of a Title (main title and sub title, not fade in, stay or fade out) in a single object
+     * @param mainTitle the top text
+     * @param subTitle the bottom text
+     */
+    public CachedTitle(String mainTitle, String subTitle) {
+        this.mainTitle = mainTitle;
+        this.subTitle = subTitle;
     }
 
     public String getMainTitle() {
@@ -52,6 +62,7 @@ public class CachedTitle {
 
     /**
      * Send the title to a single player.
+     * 1.11+ ONLY!
      *
      * @param player the player to send the title to
      */
@@ -61,6 +72,7 @@ public class CachedTitle {
 
     /**
      * Send the title to an array of players.
+     * 1.11+ ONLY!
      *
      * @param players the array of players to send the title to
      */
@@ -72,6 +84,7 @@ public class CachedTitle {
 
     /**
      * Send the title to a list of players.
+     * 1.11+ ONLY!
      *
      * @param players the list of players to send the title to
      */
@@ -79,5 +92,17 @@ public class CachedTitle {
         for (Player player : players) {
             sendToPlayer(player);
         }
+    }
+
+    /**
+     * Send the title to a player.
+     * 1.8+ ONLY!
+     * This method *may* be removed in the future as it is has been considered deprecated for multiple versions now.
+     * If you are running server version 1.11 or newer, please use the other methods instead.
+     * @param player the player to send the title to
+     */
+    @Deprecated
+    public void sendToPlayerOld(Player player) {
+        player.sendTitle(mainTitle, subTitle);
     }
 }
